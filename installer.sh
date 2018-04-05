912ae5791fb822f0411f352779ea38119101cf72
Fahad Qazi <fqazi@oxgene.com>
2018-04-05 11:42:42 +0100

    typo fix

--- installer.sh
+++ installer.sh
@@ -2,8 +2,7 @@ echo Starting Vim config installer
 
 echo Installing Pathogen
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso
-~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo pwd
 echo Installing Pluggins
