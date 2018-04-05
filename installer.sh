f74824164c57e8b7d481f5e34f75174a803b65d9
Fahad Qazi <fqazi@oxgene.com>
2018-04-05 11:33:25 +0100

    inital commit

---
+++ installer.sh
@@ -0,0 +1,10 @@
+echo Starting Vim config installer
+
+echo Installing Pathogen
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso
+~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo pwd
+echo Installing Pluggins
+
